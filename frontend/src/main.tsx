a88245665088e6a91ca28f02c74a93ba8507a701
Josh Dickson <jodicksonjoshua@gmail.com>
2026-04-04 21:59:01 +0100

    newlife

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider } from "./context/ThemeContext.tsx";
 import { CartProvider } from "./hooks/useCart.tsx";
 import { DashboardAuthProvider } from "./hooks/useDashboardUser.tsx";
 import "./index.css";
-import App from "./App";
+import App from "./App.tsx";
  
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
